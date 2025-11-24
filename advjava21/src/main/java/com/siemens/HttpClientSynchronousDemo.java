0b76e193b841cffca71855a0350d0dd3180d00d7
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-11-24 16:23:19 +0530

    updated

---
+++ advjava21/src/main/java/com/siemens/HttpClientSynchronousDemo.java
@@ -0,0 +1,43 @@
+package com.siemens;
+
+import org.json.JSONArray;
+
+import java.net.http.HttpClient;
+import java.net.http.HttpHeaders;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
+public class HttpClientSynchronousDemo {
+    public static void main(String[] args) {
+    //create http
+        HttpClient httpClient=HttpClient.newBuilder()
+                .connectTimeout(Duration.ofSeconds(100))
+                .version(HttpClient.Version.HTTP_2)
+                .build();
+        // create HttpRequest
+        HttpRequest httpRequest=HttpRequest.newBuilder()
+                .GET()
+                .uri(java.net.URI.create("https://jsonplaceholder.typicode.com/users"))
+                .build();
+        HttpResponse<String> httpResponse;
+        JSONArray jsonArray;
+        HttpHeaders httpHeaders;
+        try{
+           httpResponse=  httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+           httpHeaders= httpResponse.headers();
+              httpHeaders.map().entrySet().forEach((entry)->{
+                System.out.println(entry.getKey()+ ": " + entry.getValue());
+              });
+              jsonArray=new JSONArray(httpResponse.body());
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    System.out.println(jsonArray.getJSONObject(i).getString("name"));
+                }
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+}
