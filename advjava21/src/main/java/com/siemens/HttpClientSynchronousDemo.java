533cd8f0030eb4863dcb971a6ba531386849c785
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-11-24 16:34:13 +0530

    updated

--- advjava21/src/main/java/com/siemens/HttpClientSynchronousDemo.java
+++ advjava21/src/main/java/com/siemens/HttpClientSynchronousDemo.java
@@ -1,5 +1,8 @@
 package com.siemens;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.siemens.dtos.User;
+import com.siemens.models.Vehicle;
 import org.json.JSONArray;
 
 import java.net.http.HttpClient;
@@ -7,6 +10,7 @@ import java.net.http.HttpHeaders;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
+import java.util.List;
 
 public class HttpClientSynchronousDemo {
     public static void main(String[] args) {
@@ -21,18 +25,22 @@ public class HttpClientSynchronousDemo {
                 .uri(java.net.URI.create("https://jsonplaceholder.typicode.com/users"))
                 .build();
         HttpResponse<String> httpResponse;
-        JSONArray jsonArray;
+        //JSONArray jsonArray;
         HttpHeaders httpHeaders;
+        ObjectMapper objectMapper=new ObjectMapper();
         try{
            httpResponse=  httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
            httpHeaders= httpResponse.headers();
               httpHeaders.map().entrySet().forEach((entry)->{
                 System.out.println(entry.getKey()+ ": " + entry.getValue());
               });
-              jsonArray=new JSONArray(httpResponse.body());
+              /*jsonArray=new JSONArray(httpResponse.body());
                 for (int i = 0; i < jsonArray.length(); i++) {
                     System.out.println(jsonArray.getJSONObject(i).getString("name"));
-                }
+                }*/
+           User[] users= objectMapper.readValue(httpResponse.body(), User[].class);
+            List.of(users).forEach(System.out::println);
+
 
         }catch (Exception e){
             System.out.println(e.getMessage());
---
+++ advjava21/src/main/java/com/siemens/dtos/User.java
@@ -0,0 +1,4 @@
+package com.siemens.dtos;
+
+public record User(int id, String name, String username, String email,Object address, String phone, String website, Object company) {
+}
