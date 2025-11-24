e0e5c427d1234a3e5c6824d0672bb4421601ec12
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-11-24 12:27:51 +0530

    updated

--- advjava21/src/main/java/com/siemens/BuiltInFunctionalInterface.java
+++ advjava21/src/main/java/com/siemens/BuiltInFunctionalInterface.java
@@ -8,6 +8,7 @@ import com.siemens.models.Vehicle;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class BuiltInFunctionalInterface {
@@ -23,6 +24,10 @@ public class BuiltInFunctionalInterface {
 
        System.out.println("Age of Vehicle in Years="+ function.apply(vehicles.get(faker.number().numberBetween(0,9))));
 
+       //BiFunction
+        BiFunction<Vehicle,Vehicle,Boolean> biFunction=(v1,v2)->
+                v1.getDateOfRegistration().isBefore(v2.getDateOfRegistration());
+        System.out.println("Is Vehicle1 Registered before Vehicle2?="+ biFunction.apply(vehicles.get(faker.number().numberBetween(0,5)),vehicles.get(faker.number().numberBetween(6,9))));
 
     }
 }
