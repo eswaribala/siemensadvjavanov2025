3f5d2d2e05e4f5dcd814f6d7d0723592905d7e77
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-11-25 12:14:29 +0530

    updated

--- springcoredemo/src/main/java/com/siemens/App.java
+++ springcoredemo/src/main/java/com/siemens/App.java
@@ -3,6 +3,8 @@ package com.siemens;
 import com.siemens.models.User;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
@@ -14,6 +16,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        /*
           //IOC
         //Step1
         Resource resource = new ClassPathResource("user-config.xml");
@@ -28,7 +31,16 @@ public class App
         //actual ioc
         User user = (User) beanFactory.getBean("user");
         System.out.println(user);
+        */
 
+        ApplicationContext context = new ClassPathXmlApplicationContext("user-config.xml");
+        //getting bean from container @component
+        User user = (User) context.getBean("user");
+        //DI
+        user.setPassword("1234");
+        user.setUserName("user");
+        user.setEmail("email");
+        System.out.println(user);
 
     }
 }
--- springcoredemo/src/main/java/com/siemens/models/User.java
+++ springcoredemo/src/main/java/com/siemens/models/User.java
@@ -3,10 +3,12 @@ package com.siemens.models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Component //replaces <bean> definition in XML
 public class User {
     private String userName;
     private String password;
