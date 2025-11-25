d9267f601c038c37f6631f046823f428abb3a702
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-11-25 12:00:58 +0530

    updated

--- springcoredemo/src/main/java/com/siemens/App.java
+++ springcoredemo/src/main/java/com/siemens/App.java
@@ -1,5 +1,11 @@
 package com.siemens;
 
+import com.siemens.models.User;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,21 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+          //IOC
+        //Step1
+        Resource resource = new ClassPathResource("user-config.xml");
+        //Step2
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        //Step3
+        XmlBeanDefinitionReader xmlBeanDefinitionReader = new XmlBeanDefinitionReader(beanFactory);
+
+        xmlBeanDefinitionReader.loadBeanDefinitions(resource);
+        //Step4
+        //accessing the bean
+        //actual ioc
+        User user = (User) beanFactory.getBean("user");
+        System.out.println(user);
+
+
     }
 }
