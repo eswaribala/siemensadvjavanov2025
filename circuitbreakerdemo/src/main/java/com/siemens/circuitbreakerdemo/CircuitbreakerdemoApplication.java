4543257fbc2cc3d71fbbcd2cc32b17567ece6c08
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-11-27 12:27:39 +0530

    updated

---
+++ circuitbreakerdemo/src/main/java/com/siemens/circuitbreakerdemo/CircuitbreakerdemoApplication.java
@@ -0,0 +1,13 @@
+package com.siemens.circuitbreakerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircuitbreakerdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CircuitbreakerdemoApplication.class, args);
+    }
+
+}
---
+++ circuitbreakerdemo/src/test/java/com/siemens/circuitbreakerdemo/CircuitbreakerdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siemens.circuitbreakerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitbreakerdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
