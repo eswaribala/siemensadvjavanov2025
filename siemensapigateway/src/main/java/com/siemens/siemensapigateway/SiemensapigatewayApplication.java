3ed4b15ca989762d5d0187ade1a8d87ab6d9925c
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-11-27 11:33:41 +0530

    updated

---
+++ siemensapigateway/src/main/java/com/siemens/siemensapigateway/SiemensapigatewayApplication.java
@@ -0,0 +1,13 @@
+package com.siemens.siemensapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiemensapigatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiemensapigatewayApplication.class, args);
+    }
+
+}
---
+++ siemensapigateway/src/test/java/com/siemens/siemensapigateway/SiemensapigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siemens.siemensapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiemensapigatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
