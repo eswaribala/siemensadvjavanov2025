b31a7d66935efcf5c15a240149b41f5a27540f2f
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-11-28 10:32:11 +0530

    updated

---
+++ policyapi/src/main/java/com/siemens/policyapi/PolicyapiApplication.java
@@ -0,0 +1,13 @@
+package com.siemens.policyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicyapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolicyapiApplication.class, args);
+    }
+
+}
---
+++ policyapi/src/test/java/com/siemens/policyapi/PolicyapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siemens.policyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicyapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
